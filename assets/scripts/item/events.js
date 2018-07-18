ee22ad60a9c563cbe438611519965e94d96f6d4a
swholm19 <swholm19@gmail.com>
2018-07-18 09:25:04 -0400

    Fixed Bug that cleared inventory after new create item
    -Events.js: added .then to call getAll items that repopulates
    inventory list
    -Ui.js: on successful create modal closes and clears form.

--- assets/scripts/item/events.js
+++ assets/scripts/item/events.js
@@ -6,10 +6,9 @@ const itemApi = require('./api.js')
 const onCreateItem = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
-  console.log('data.item is ', data.item)
-  console.log('event.target is ', event.target)
   itemApi.createItem(data)
     .then(itemUi.createItemSuccess)
+    .then(onGetItems)
     .catch(itemUi.createItemError)
 }
 const onGetItems = () => {
--- assets/scripts/item/ui.js
+++ assets/scripts/item/ui.js
@@ -4,12 +4,8 @@ const store = require('../store')
 const showItemsTemplate = require('../templates/item-listing.handlebars')
 
 const createItemSuccess = function (data) {
-  store.item = data.item
-  console.log('data is ', data)
-  console.log('data.item is', data.item)
-  console.log('data.item.name is', data.item.name)
-  const showNewItemsHtml = showItemsTemplate({ items: data.item })
-  $('#inventoryList').html(showNewItemsHtml)
+  $('#createItemModal').modal('hide')
+  $('#create-item-form')[0].reset()
 }
 const createItemError = function () {
   $('#createModalLabel').html('Something went wrong try again!')

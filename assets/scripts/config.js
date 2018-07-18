2c1e0d658331e6878813baf10fd23809e5a9e51b
KatherineWard <katherine.eileen.ward@gmail.com>
2018-07-18 15:41:52 -0400

    Fixed typo in Heroku URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://ims-project-api.herokuapp.com/',
+  production: 'https://ims-project-api.herokuapp.com',
   development: 'http://localhost:4741'
 }
 

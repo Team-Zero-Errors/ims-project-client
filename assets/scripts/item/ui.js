8dccaa9f523fa806c7d9679101dfa716df4a30a2
swholm19 <swholm19@gmail.com>
2018-07-19 09:54:39 -0400

    Fixed Bug: All Modals Showing Update Title Message
    -In handlebars added new class unique to update modal that i target
    when resetting the title message
    -In the ui.js in jQuery request updated class that it targets to
    updated modal title

--- assets/scripts/item/ui.js
+++ assets/scripts/item/ui.js
@@ -39,13 +39,13 @@ const updateItemSuccess = function (itemId) {
   $('.update-form')[0].reset()
 }
 const updateItemError = function () {
-  $('.modal-title').css('color', 'red')
-  $('.modal-title').html('Something went wrong creating item try again!')
+  $('.itemUpdateTitle').css('color', 'red')
+  $('.itemUpdateTitle').html('Something went wrong creating item try again!')
 }
 
 const resetUiHandleing = function () {
-  $('.modal-title').css('color', 'black')
-  $('.modal-title').html('Update Item')
+  $('.itemUpdateTitle').css('color', 'black')
+  $('.itemUpdateTitle').html('Update Item')
   $('#itemTitle').css('color', 'white')
   $('#itemTitle').html('Item Inventory')
   $('#createModalLabel').css('color', 'black')
